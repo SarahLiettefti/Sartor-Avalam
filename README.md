364bcef9bc0e6dcf8554c37037d522499fe54a2c
Sarah Liettefti <sarah.liettefti@gmail.com>
2020-05-07 11:09:29 +0200

    Mise à jour readme

--- README.md
+++ README.md
@@ -9,46 +9,22 @@ Voir règles du projet : https://github.com/ECAM-Brussels/AIGameRunner/blob/mast
 - cherypy, cherrypy.lib.static
 - random : pour choisir un coup dans une liste
 
+## Inscription au server et démarrage du jeu
+Afin de démarrer le server vous avez besoin des fichier :
+- Sartor.py
+- simple.py
+- jeu1. py
+dans un même dossier sur votre ordinateur.
 
-## Lancement de l'ia contre une autre ia:
-
-### Inscription au server et démarrage du jeu
-
-
-#### Fonctionnement : 
-Le fichier simple.py permet l'inscription, il envoie un message TCP en JSON au porte 3001 sous cette forme :
-
-```json
-{
-	"matricules": ["18253", "18322"],
-	"port": port ,
-	"name": "Sartor"
-}
-```
-Utiliser la commande:
-```
-python simple.py port
-```
-Où `port` doit être remplacé par le numéro de port utilisé par l'ia, sinon il prendra le port 8001 par défaut. 
-
-### Démarrer le jeu
-
-Pour lancer l'ia assurez-vous que le fichier `jeu1.py` est bien téléchargé et se trouve dans le même dossier. 
-Écrivez dans un terminal la commande:
+Utiliser cette commande dans le terminal:
 ```
 python Sartor.py port
 ```
-Remplacez `port` par le numéro de port utilisé par l'ia, sinon il prendra le port 8001 par défaut. 
+Où `port` doit être remplacé par le numéro de port utilisé par l'ia, sinon il prendra le port 8001 par défaut. 
 
-## Explication jeu.1
+## Stratégie
 
-Via la route `casejouables`, l'ia va d'abord créer une liste `casesoccupes` de toutes les cases de l'état du jeu sur lesquels il est possible de se déplacé (c'est à dire les cases occupées par au moins un pion et au plus 4 pions, puisque lorsqu'une tour a 5 pions elle est dite figée). Chaque case du jeu est identifiée par une liste avec en premier son numéro de ligne et en deuxième son numéro de colonne ([ligne,colonne])
-
-La route `coup` va regarder pour chaque case dans `casesoccupes` si un déplacement est possible (si des cases voisines se trouvent aussi dans la liste `casesoccupes` et si la sommes de leurs pions n'est pas supérieur à 5). Si c'est le cas, la case d'origine, appelé `de` et la case de destination, appelé `to`, sont envoyé vers la fonction `findallcoup` qui va trier les déplacements qu'elle reçoit dans des listes en fonction de leur objectif.
-
-### Listes de stratégie: 
-Classé de la moins avantageuse à la plus avantageuse :
-
+Pour élaborer notre stratégie nous avons joué à Avalam et noté des catégories de coups intéressants. Pour pouvoir les appliquer à notre code, il reprend à chaque tours l’état du jeu actuel ainsi que le liste des joueurs de la partie afin de savoir quel couleur nous défendons. Grâce à l’état du jeu il trie chaque déplacement possible dans une liste qui lui correspond. Le code va ensuite prendre au hasard un coup dans la liste la plus profitable non vide. Vous trouverez ci-dessous en tableau présentant les listes dans l’ordre inverse renvoyer par le code :
 | Listes | Objectif | Commentaire |
 |----------|:-------------:|------:|
 | `coups` | liste de tous les déplacements possibles | elle est utilisée si toutes les autres listes sont vide, elle permet de ne pas renvoyer un déplacement libre si il ne trouve pas de bon coup |
@@ -58,8 +34,31 @@ Classé de la moins avantageuse à la plus avantageuse :
 | `ecrase` | déplace une de nos tours sur l'adversaire|remplacé par dans l'ordre: `ecrase4`, `ecrase3` et `ecrase2`|
 | `ecrase4`, `ecrase3` et `ecrase2` | dérivé de la liste `ecrase`, le chiffre correspond à la hauteur de la tour final créé sur la tour adverse| il est plus avantageux de d'abord créer plein de petites tours de 2 avant des tours de 4 |
 | `complettheybyus` | déplace une de nos tours sur l'adversaire et créons une tour de 5 | nous sécurise un point |
-| `ensembleseul` | liste de déplacements dont le pion d'origine nous appartient et le pion destination non dont les deux n'ont pas d'autres choix de déplacement | nous sécurise un point | 
+| `ensembleseul` | liste de déplacements dont le pion d'origine nous appartient et le pion destination non dont les deux n'ont pas d'autres choix de déplacement | nous sécurise un point |
 
+En résumé notre plan est de mettre le plus de pion de notre couleur possible en haut des tours. C’est une stratégie d’attaque plus que de défense. 
+
+
+## Fonctionnement/Détails : 
+### simple.py
+Le fichier simple.py permet l'inscription, il envoie un message TCP en JSON au porte 3001 sous cette forme :
+
+```json
+{
+	"matricules": ["18253", "18322"],
+	"port": port ,
+	"name": "Sartor"
+}
+```
+Où `port` doit être remplacé par le numéro de port utilisé par l'ia, sinon il prendra le port 8001 par défaut. 
+
+### Explication jeu1.py
+
+Via la route `casejouables`, l'ia va d'abord créer une liste `casesoccupes` de toutes les cases de l'état du jeu sur lesquels il est possible de se déplacé (c'est à dire les cases occupées par au moins un pion et au plus 4 pions, puisque lorsqu'une tour a 5 pions elle est dite figée). Chaque case du jeu est identifiée par une liste avec en premier son numéro de ligne et en deuxième son numéro de colonne ([ligne,colonne])
+
+La route `coup` va regarder pour chaque case dans `casesoccupes` si un déplacement est possible (si des cases voisines se trouvent aussi dans la liste `casesoccupes` et si la sommes de leurs pions n'est pas supérieur à 5). Si c'est le cas, la case d'origine, appelé `de` et la case de destination, appelé `to`, sont envoyé vers la fonction `findallcoup` qui va trier les déplacements qu'elle reçoit dans des listes en fonction de leur objectif.
+
+(voir tableau plus haut)
 
 Une fois toutes les cases parcourues, ces listes sont renvoyées  vers `Choosestrat` qui renvoie la liste la plus avantageuse non vide dans laquelle un déplacement sera choisi aléatoirement, mis en forme de dictionnaire comme ci-dessous avant d'être renvoyé sous forme de json par le server `Sarator` : 
 
@@ -73,3 +72,16 @@ Une fois toutes les cases parcourues, ces listes sont renvoyées  vers `Choosest
 
 ## Jouer avec random:
 Vous pouvez jouer en aléatoire grâce au server `randomplayer.py`. 
+Afin de démarrer le server vous avez besoin des fichier :
+- randomplayer.py
+- simple.py
+- jeurandom. py
+dans un même dossier sur votre ordinateur.
+Utiliser cette commande dans le terminal:
+```
+python randomplayer.py port
+```
+Où `port` doit être remplacé par le numéro de port utilisé par l'ia, sinon il prendra le port 8002 par défaut. 
+
+### Fonctionnement:
+Le code `jeurandom.py`est une version simplifier de `jeu1.py`. Il ne créer qu'un liste : `coups` et y prends un déplacemetn au hasard.
--- jeu1.py
+++ jeu1.py
@@ -1,73 +1,23 @@
 import random
 
-def casejouables(body):
-    game=body["game"]
-    casesoccupes = []#toutes cases "jouable" #au début 48
-    li = 0
-    for game[li] in game:
-        col = 0
-        for game[li][col] in game[li]:
-            if (0<len(game[li][col])<5):
-                casesoccupes.append([li,col])
-            col+=1
-        li+=1
-    return casesoccupes
+def themove(body):
+    m = hasard(body)
+    moves = turningoodform(m)
+    return moves
 
-
-def wichplayer(body):#renvoie la couleur joué
-    if (body["players"][0]==body["you"]): 
-        return 0
-    else: 
-        return 1
+def hasard(body):#donne un coup au hasard sous la forme [[0,3],[0,4]]
+    m=random.choice(coup(body))
+    return m
 
 def turningoodform(m):#transforme un [[0,3],[0,4]] en dictionnaire de la bonne forme
     moves = {"move": {"from": m[0], "to": m[1]}}
     return moves
 
-def Choosestrat(coups,ecrase2,ecrase3,ecrase4,ajout,embete,completusbyus,complettheybyus,complettheybythey,ensembleseul):#renvoie la liste de la meilleur strategie
-    if len(ensembleseul)!=0:
-        return ensembleseul
-    elif len(complettheybyus)!=0:
-        return complettheybyus
-    elif len(ecrase2)!=0:
-        return ecrase2
-    elif len(ecrase3)!=0:
-        return ecrase3
-    elif len(ecrase4)!=0:
-        return ecrase4  
-    elif len(complettheybythey)!=0:
-        return complettheybythey
-    elif len(embete)!=0:
-        return embete
-    elif len(ajout)!=0:
-        return ajout
-    else:
-        return coups
-
-def findallcoup(game,de,to,coups,ecrase,ecrase2,ecrase3,ecrase4,ajout,embete,player,completusbyus,complettheybyus,complettheybythey):
-    move = [de,to]
-    coups.append(move)
-    if game[de[0]][de[1]][-1]==player:#si le pion qu'on va déplacer est a nous
-        if game[to[0]][to[1]][-1]==player:#si on le déplace sur un de nos pion
-            ajout.append(move)
-            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: #si ça complète une tour de 5 pion
-                completusbyus.append(move)
-        else:#si on le déplace sur un pion ennemi
-            ecrase.append(move)
-            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: 
-                complettheybyus.append(move)
-            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==2: 
-                ecrase2.append(move)
-            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==3: 
-                ecrase3.append(move)
-            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==4: 
-                ecrase4.append(move)
-    else:#si le pion qu'on va déplacer est à l'ennemi
-        if game[to[0]][to[1]][-1]!=player:
-            embete.append(move)
-            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: 
-                complettheybythey.append(move)
-    return coups, ecrase,ecrase2,ecrase3,ecrase4, ajout, embete, completusbyus,complettheybyus,complettheybythey
+def wichplayer(body):#renvoie la couleur jouée
+    if (body["players"][0]==body["you"]): 
+        return 0
+    else: 
+        return 1
 
 def coup(body):
     casesoccupes= casejouables(body)
@@ -75,17 +25,17 @@ def coup(body):
     player=wichplayer(body)
     coups=[] #tous les coups possible
     ecrase=[]#met un de nos pions sur l'autre
-    ecrase2=[] #mets un de nos pions sur l'autre et creer un tour de 2
-    ecrase3=[] #mets un de nos pions sur l'autre et creer un tour de 3
-    ecrase4=[] #mets un de nos pions sur l'autre et creer un tour de 4
+    ecrase2=[] #met un de nos pions sur l'autre et creer un tour de 2
+    ecrase3=[] #met un de nos pions sur l'autre et creer un tour de 3
+    ecrase4=[] #met un de nos pions sur l'autre et creer un tour de 4
     ajout=[]#met un de nos pions sur un notre a nous
     embete=[]#met un des pions adversaire sur un autre a lui
-    completusbyus = [] #complet un tour de 5 par nos couleur
-    complettheybyus = [] #complet un tour de 5 par les couleur ennemie (apres l'avoir completé ce sont des pions ennemis qui ne savent plus bouger)
+    completusbyus = [] #complète un tour de 5 par notre couleur
+    complettheybyus = [] #complète un tour de 5 par la couleur ennemie (apres l'avoir completé ce sont des pions ennemis qui ne savent plus bouger)
     complettheybythey = []
-    seul=[]#liste de déplacement dont l'origine ne peut faire que ce déplacement
+    seul=[]#liste de déplacements dont l'origine ne peut faire que ce déplacement
     seulvoisin=[] #une liste des cases qui n'ont qu'un déplacement possible
-    ensembleseul=[]#déplacement d'une case qui ne peut que etre deplacée qu'a un endroit et qui écrase l'adversaire sur une tour qui ne pouvait aussi etre déplacer que à cet endroit
+    ensembleseul=[]#déplacement d'une case qui ne peut que être deplacée à un endroit et qui écrase l'adversaire sur une tour qui ne pouvait aussi être déplacé que à cet endroit
     for line in range(9):
         for col in range(9):
             de=[line,col]
@@ -108,11 +58,60 @@ def coup(body):
                 ensembleseul.append(maybebest)
     return Choosestrat(coups,ecrase2,ecrase3,ecrase4,ajout,embete,completusbyus,complettheybyus,complettheybythey,ensembleseul) #renvoie une liste à la fin
 
-def hasard(body):#donne un coup au hasard sous la forme [[0,3],[0,4]]
-    m=random.choice(coup(body))
-    return m
+def casejouables(body):
+    game=body["game"]
+    casesoccupes = []#toutes cases "jouables" #au début 48
+    li = 0
+    for game[li] in game:
+        col = 0
+        for game[li][col] in game[li]:
+            if (0<len(game[li][col])<5):
+                casesoccupes.append([li,col])
+            col+=1
+        li+=1
+    return casesoccupes
 
-def themove(body):
-    m = hasard(body)
-    moves = turningoodform(m)
-    return moves
+def findallcoup(game,de,to,coups,ecrase,ecrase2,ecrase3,ecrase4,ajout,embete,player,completusbyus,complettheybyus,complettheybythey):
+    move = [de,to]
+    coups.append(move)
+    if game[de[0]][de[1]][-1]==player:#si le pion qu'on va déplacer est à nous
+        if game[to[0]][to[1]][-1]==player:#si on le déplace sur un de nos pions
+            ajout.append(move)
+            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: #si ça complète une tour de 5 pions
+                completusbyus.append(move)
+        else:#si on le déplace sur un pion ennemi
+            ecrase.append(move)
+            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: 
+                complettheybyus.append(move)
+            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==2: 
+                ecrase2.append(move)
+            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==3: 
+                ecrase3.append(move)
+            elif ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==4: 
+                ecrase4.append(move)
+    else:#si le pion qu'on va déplacer est à l'ennemi
+        if game[to[0]][to[1]][-1]!=player:
+            embete.append(move)
+            if ((len(game[de[0]][de[1]]))+(len(game[to[0]][to[1]])))==5: 
+                complettheybythey.append(move)
+    return coups, ecrase,ecrase2,ecrase3,ecrase4, ajout, embete, completusbyus,complettheybyus,complettheybythey
+
+def Choosestrat(coups,ecrase2,ecrase3,ecrase4,ajout,embete,completusbyus,complettheybyus,complettheybythey,ensembleseul):#renvoie la liste de la meilleur strategie
+    if len(ensembleseul)!=0:
+        return ensembleseul
+    elif len(complettheybyus)!=0:
+        return complettheybyus
+    elif len(ecrase2)!=0:
+        return ecrase2
+    elif len(ecrase3)!=0:
+        return ecrase3
+    elif len(ecrase4)!=0:
+        return ecrase4  
+    elif len(complettheybythey)!=0:
+        return complettheybythey
+    elif len(embete)!=0:
+        return embete
+    elif len(ajout)!=0:
+        return ajout
+    else:
+        return coups
